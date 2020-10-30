c2bd600e5632a4b42b1c3c537babd7a1cdfc03a7
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2020-10-29 20:24:57 -0400

    upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=dIPTOgpTvbwbpCqbGFo19S6OgltyAk3IW7-n9qcI07U";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -11,21 +11,28 @@ const doCORSRequest = (options) => {
   return x;
 };
 
-// Example promise that executes the GET request above and waits for it to finish before resolving
-const corsPromise = () =>
-  new Promise((resolve, reject) => {
-    const request = doCORSRequest({
-      url: "https://trefle.io/api/v1/plants" + apiToken,
-    });
-    resolve(request);
+
+new Promise((resolve) => {
+  const request = doCORSRequest({
+    url: "https://trefle.io/api/v1/plants" + apiToken + "&filter_not[image_url]=null",
   });
-
-// THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
-corsPromise().then(
-  (request) =>
-    (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
-    })
-);
-
-//// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+  resolve(request);
+})
+  .then((request) => {
+    request.onload = (response) => {
+      const trefleData = JSON.parse(response.originalTarget.response)["data"];
+      const descriptions = trefleData.map((e) => {
+        console.log(e);
+        const img = "<img src=\"" + e["image_url"] + "\" alt=\"" + e["scientific_name"] + "\"/>";
+        const des = "<figcaption><em>" + e["scientific_name"] + "</em></figcaption>";
+        return "<figure>" + img + des + "</figure>";
+      })
+      const ulElem = document.querySelector("#plants");
+      ulElem.innerHTML = "";
+      descriptions.map((d) => {
+        const liElem = document.createElement('li');
+        liElem.innerHTML = d;
+        ulElem.appendChild(liElem);
+      })
+    }
+  });
